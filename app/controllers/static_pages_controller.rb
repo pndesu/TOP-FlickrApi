a4dd3c8675de52dec4133d0563c195f50499893f
pndesu <tranvanphucnguyen1@gmail.com>
2023-02-15 00:16:19 +1100

    Add search photo based on user id

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,12 @@
 class StaticPagesController < ApplicationController
+    require 'flickr'
+
+    def index
+        flickr = Flickr.new(ENV['flickr_api_key'], ENV['flickr_shared_secret'])
+        user_id = params[:user_id]
+        begin
+            @photo_urls = flickr.people.getPublicPhotos(user_id: user_id)
+        rescue Flickr::FailedResponse => e
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "static_pages#index"
+  resources :static_pages
 end
